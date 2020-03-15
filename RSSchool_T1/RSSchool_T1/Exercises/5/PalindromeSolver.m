d9fff4c1e8a4870dd96010a696f414d965b611ca
andrei.kishkevich <bydron@gmail.com>
2020-03-15 16:57:50 +0300

    feat: add solution for fifth task

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -4,7 +4,40 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    NSMutableArray *arr = [PalindromeSolver parseStrToArr: s];
+    int count = 0;
+    long prevMax = 0;
+
+    for (int i = ([n intValue] / 2); i >= 0; i -= 1) {
+
+        NSInteger leftElem = [[arr objectAtIndex: i] integerValue];
+        NSInteger rightElem = [[arr objectAtIndex: ([n intValue] - i - 1) ] integerValue];
+        if (leftElem != rightElem) {
+            prevMax = MAX(prevMax, MAX(leftElem, rightElem));
+            arr[i] = [NSString stringWithFormat:@"%ld", prevMax];
+            arr[([n intValue] - i - 1)] = [NSString stringWithFormat:@"%ld", prevMax];
+
+            if (leftElem != prevMax) {
+                count += 1;
+            }
+
+            if (rightElem != prevMax) {
+                count += 1;
+            }
+        }
+    }
+
+    return  count == [k intValue] ? [arr componentsJoinedByString:@""] : @"-1";
+}
+
++ (NSMutableArray*)parseStrToArr: (NSString *)str {
+    NSMutableArray *arrStr = [[NSMutableArray alloc] initWithArray:@[]];
+    NSUInteger strLength = [str length];
+    for (int i = 0; i < strLength; i++) {
+        NSString *ch = [str substringWithRange:NSMakeRange(i, 1)];
+        [arrStr addObject:ch];
+    }
+    return arrStr;
 }
 
 @end
