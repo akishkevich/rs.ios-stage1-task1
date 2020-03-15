099b165305c3366ce86c6b11f2e28a93605d482a
andrei.kishkevich <bydron@gmail.com>
2020-03-15 17:22:23 +0300

    feat: add solution for fourth task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,62 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSUInteger strLength = [string length];
+    if (!string || strLength == 0) { return @[]; }
+
+    NSMutableArray<NSNumber *> *circleStr = [@[] mutableCopy];
+    NSMutableArray<NSNumber *> *squareStr = [@[] mutableCopy];
+    NSMutableArray<NSNumber *> *triangeStr = [@[] mutableCopy];
+
+    NSMutableArray<NSString *> *result = [@[] mutableCopy];
+    NSMutableArray<NSValue *> *ranges = [@[] mutableCopy];
+
+    for (int i = 0; i < strLength; i++) {
+        unichar character = [string characterAtIndex:i];
+        NSNumber *posBracket = nil;
+        switch(character) {
+            case '(':
+                [circleStr addObject:@(i)];
+                break;
+            case '[':
+                [squareStr addObject:@(i)];
+                break;
+            case '<':
+                [triangeStr addObject:@(i)];
+                break;
+            case ')':
+                posBracket = [circleStr lastObject];
+                [circleStr removeLastObject];
+                break;
+            case ']':
+                posBracket = [squareStr lastObject];
+                [squareStr removeLastObject];
+                break;
+            case '>':
+                posBracket = [triangeStr lastObject];
+                [triangeStr removeLastObject];
+                break;
+        }
+
+        if (!posBracket) { continue; }
+
+        NSRange range = NSMakeRange(
+            [posBracket intValue] + 1,
+            i - [posBracket intValue] - 1
+        );
+        [ranges addObject:[NSValue valueWithRange:range]];
+    }
+
+    [ranges sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        return [obj1 rangeValue].location > [obj2 rangeValue].location;
+    }];
+
+    for (NSValue *value in ranges) {
+        NSString *subString = [string substringWithRange:[value rangeValue]];
+        if ([subString length]) { [result addObject: subString]; }
+    }
+
+    return result;
 }
 
 @end
